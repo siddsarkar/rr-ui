e26c39b9837eaf08fc10a5f595b529a935c9a14e
Siddhartha Sarkar <siddsarkar7@gmail.com>
2021-03-11 17:23:00 +0530

    added home view

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,13 @@
 import "./App.scss";
 import NavBar from "./components/navbar/navbar";
 import logo from "../assets/logo.png";
-import { useEffect, useState } from "react";
+import chevronUp from "../assets/up-chevron.svg";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
+  const topRef = useRef(null);
 
   const getData = () => {
     const requestURL = `https://www.readingright.in/apiservice/article/read/?format=json&user_id=57`;
@@ -23,30 +26,57 @@ function App() {
   useEffect(() => {
     getData().then((json) => {
       setArticles(json.data);
-
-      console.log(json.data);
+      setLoading(false);
     });
   }, []);
 
   return (
     <div className="App">
+      <div ref={topRef} />
       <NavBar logo={logo} />
-
       <div className="App-main">
-        {articles.map((article) => {
-          return (
-            <div id={article.id}>
-              <img
-                src={"https://www.readingright.in/" + article.art_image}
-                alt="mkm"
-              />
-              <div dangerouslySetInnerHTML={{ __html: article.art_data }}></div>
-            </div>
-          );
-        })}
+        {loading ? (
+          <div className="loader"></div>
+        ) : (
+          articles.map((article) => {
+            return (
+              <div className="article" key={article.id}>
+                <img
+                  className="article-cover"
+                  src={"https://www.readingright.in/" + article.art_image}
+                  alt="mkm"
+                />
+                <div className="article-meta">
+                  <div>The Caravan | 2 Weeks Ago</div>
+                  <p>
+                    Elon Musk is known to have a not-so-conventional view of
+                    things. It comes as no surprise that the Tesla CEO isn’t a
+                    ‘fan’ of the traditional form of working. Elon Musk is known
+                    to have a not-so-conventional view of things. It comes as no
+                    surprise that the Tesla CEO isn’t a ‘fan’ of the traditional
+                    form of working…
+                  </p>
+                  <div>Science & Technology</div>
+                </div>
+              </div>
+            );
+          })
+        )}
       </div>
-
-      <div className="App-footer">Footer</div>
+      {articles.length > 0 && (
+        <div className="btn-container">
+          <div className="more-btn">View More</div>
+        </div>
+      )}
+      <div className="App-footer">
+        <div
+          className="top-btn"
+          onClick={() => topRef.current.scrollIntoView({ behaviour: "smooth" })}
+        >
+          <img src={chevronUp} alt="^"></img>
+        </div>
+        <p> Ⓒ 2020 - 2021 Reading Right. All rights reserved.</p>
+      </div>
     </div>
   );
 }
--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,19 +1,23 @@
+import menuIcon from "../../../assets/menu.svg";
+import searchIcon from "../../../assets/search.svg";
+
 export default function NavBar({ logo }) {
   return (
     <div className="App-header">
       <div className="App-header-actions">
-        <div className="menu">Menu</div>
+        <div className="menu">
+          <img src={menuIcon} alt="m"></img>
+        </div>
         <img className="logo" src={logo} alt="Logo" />
-        <div className="search">search</div>
+        <div className="search">
+          <img src={searchIcon} alt="s"></img>
+        </div>
       </div>
       <div className="App-header-links">
-        <p className="link">Editorials</p>
-        <p className="link">Editorials</p>
-        <p className="link">Editorials</p>
-        <p className="link">Editorials</p>
-        <p className="link">Editorials</p>
-        <p className="link">Editorials</p>
-        <p className="link">Editorials</p>
+        <p className="link">Editorials |</p>
+        <p className="link">Business |</p>
+        <p className="link">Science & Technology |</p>
+        <p className="link">Economy</p>
       </div>
     </div>
   );
